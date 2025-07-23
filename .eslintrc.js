debfe743f98d30995a9e919a9651fd8b226cf58b
Earntor <arengrigorjan@gmail.com>
2025-07-23 23:10:04 +0200

    .eslintrc.js toegevoegd

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+// .eslintrc.js
+module.exports = {
+  extends: 'next',
+  rules: {
+    '@next/next/no-html-link-for-pages': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    '@next/next/no-img-element': 'off',
+  },
+};
