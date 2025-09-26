e95fc5006ece23d4c518509779884bde84fd7c7d
Earntor <arengrigorjan@gmail.com>
2025-09-27 00:28:15 +0200

    formattimestap update

--- src/lib/formatTimestamp.js
+++ src/lib/formatTimestamp.js
@@ -3,3 +3,4 @@ export function formatDutchDateTime(date) {
   const dt = typeof date === "string" ? new Date(date) : date;
   return dt.toLocaleString("nl-NL", { timeZone: "Europe/Amsterdam" });
 }
+ 
